0ac76e40d52b2491e06c7536308f78c09eca960c
Noe-C <noe91.carranza@gmail.com>
2023-02-28 16:32:30 -0300

    sw.js agregado de nuevo

--- sw.js
+++ sw.js
@@ -1,58 +1,57 @@
-const cacheName = "cache1";
+const cacheName = "cache1"; 
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener("install", event => {
 
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index-1.html",
-        "/css/styles.css",
-        "/js/index.js",
-        "/ballon-heart.png",
-        "/ballon-heart.svg",
-        "/cookie.svg",
-        "imagen 1.jpg",
-        "aviso-cookies.js",
-        "estilos.css",
-        "formulario.js",
-        "heart-solid.png",
-        "heart-solid.svg",
-        "manifest.json",
-        "script.js",
-        "toast.js",
-      ]);
-    })
-  );
+	self.skipWaiting();
+
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll([
+                "/",
+                "/index-1.html",
+                "/css/styles.css",
+                "/js/index.js",
+                "/ballon-heart.png",
+                "/ballon-heart.svg",
+                "/cookie.svg",
+                "imagen 1.jpg",
+                "aviso-cookies.js",
+                "estilos.css",
+                "formulario.js",
+                "heart-solid.png",
+                "heart-solid.svg",
+                "manifest.json",
+                "script.js",
+                "toast.js",
+			]);
+		})
+	);
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      Promise.all(
-        keys.map((key) => {
-          if (![cacheName].includes(key)) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate", event => {
+
+	event.waitUntil(
+		caches.keys().then(keys => {
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		})
+	);
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.open(cacheName).then((cache) => {
-      return cache.match(event.request).then((response) => {
-        return (
-          response ||
-          fetch(event.request).then((networkResponse) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          })
-        );
-      });
-    })
-  );
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
 });
