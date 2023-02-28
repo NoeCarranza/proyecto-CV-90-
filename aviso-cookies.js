554bba912367afd884b870338271a4fd94fee2d6
Noe-C <noe91.carranza@gmail.com>
2023-02-28 00:06:27 -0300

    ultima correccion 28/2 00hrs

--- aviso-cookies.js
+++ aviso-cookies.js
@@ -1,22 +1,21 @@
-const botonAceptarCookies = document.getElementById ('btn-aceptar-cookies');
-const avisoCookies = document.getElementById ('aviso-cookies');
-const fondoAvisoCookies = document.getElementById ('fondo-aviso-cookies');
-
-
-if(!localStorage.getItem('cookies-aceptadas')){
-    avisoCookies.classList.add('active');
-    fondoAvisoCookies.classList.add('active');
-}else{
-    dataLayer.push({'event': 'cookies-aceptadas'});
-}
-
-
-
-botonAceptarCookies.addEventListener ('click', () => {
-    avisoCookies.classList.remove ('active');
-    fondoAvisoCookies.classList.remove ('active');
-
-    localStorage.setItem('cookies-aceptadas', true);
-
-    dataLayer.push({'event': 'cookies-aceptadas'});
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const avisoCookies = document.getElementById('avisocookies');
+    const fondo = document.getElementById('fondoavisocookies');
+    const botonAceptar = document.getElementById('btnaceptarcookies');
+    var dataLayer = window.dataLayer || [];
+  
+    if (!localStorage.getItem('cookies-aceptadas')) {
+      avisoCookies.classList.add('activo');
+      fondo.classList.add('activo');
+    } else {
+      dataLayer.push({ 'event': 'cookies-aceptadas' });
+    }
+  
+    botonAceptar.addEventListener('click', () => {
+      avisoCookies.classList.remove('activo');
+      fondo.classList.remove('activo');
+  
+      localStorage.setItem('cookies-aceptadas', true);
+      dataLayer.push({ 'event': 'cookies-aceptadas' });
+    });
+  });
